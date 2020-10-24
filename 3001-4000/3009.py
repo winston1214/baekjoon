c1891e4fcdcdc3b18912b7c2e5cfd7be0e552502
winston1214 <winston1214@naver.com>
2020-10-24 16:54:12 +0900

    commit

---
+++ 3001-4000/3009.py
@@ -0,0 +1,12 @@
+import sys
+a_x,a_y = map(int,sys.stdin.readline().split())
+b_x,b_y = map(int,sys.stdin.readline().split())
+c_x,c_y = map(int,sys.stdin.readline().split())
+x = [a_x,b_x,c_x]
+y = [a_y,b_y,c_y]
+for i in x:
+    if x.count(i) == 1:
+        print(i,end = ' ')
+for j in y:
+    if y.count(j) == 1:
+        print(j)
