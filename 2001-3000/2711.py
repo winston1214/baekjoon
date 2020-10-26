ae8d60cfeb0416b4d725b66a7e4b9cb9064566a8
winston1214 <winston1214@naver.com>
2020-10-26 22:47:22 +0900

    commit

---
+++ 2001-3000/2711.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline())
+for _ in range(N):
+    num , string = sys.stdin.readline().split()
+    ls = list(string)
+    ls = ls[:int(num)-1] + ls[int(num):]
+    print(''.join(ls))
+    
---
+++ 5001-6000/5717.py
@@ -0,0 +1,6 @@
+import sys
+while True:
+    a,b=map(int,sys.stdin.readline().split())
+    if a==b==0:
+        break
+    else:print(a+b)
