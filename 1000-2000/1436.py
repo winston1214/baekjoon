56e9e376983a3086cb82d05b3bf417e71f9bf9b5
winston1214 <winston1214@naver.com>
2020-12-20 23:24:31 +0900

    commit

---
+++ 1000-2000/1436.py
@@ -0,0 +1,11 @@
+import sys
+x = int(sys.stdin.readline())
+cnt = 0
+num = 666
+while True:
+    if '666' in str(num):
+        cnt+=1
+    if cnt == x:
+        print(num)
+        break
+    num+=1
